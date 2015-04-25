a5e870b85266a0dbc669d9bd00474e39e167f6f9
Johannes Spielmann <j@spielmannsolutions.com>
2015-04-25 15:13:45 +0200

    Sortierung

--- minibay/auctions/views.py
+++ minibay/auctions/views.py
@@ -7,7 +7,12 @@ from .models import Auction
 def main(request):
     context = {}
 
-    auctions = Auction.objects.filter(end_date__gt=timezone.now())
+    auctions = Auction.objects.filter(
+        end_date__gt=timezone.now()
+    ).order_by(
+        'end_date', '-current_amount',
+    )
+
     context['auctions'] = auctions
-    
+
     return render(request, 'auctions/main.djhtml', context)
