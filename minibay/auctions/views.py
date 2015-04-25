99cff23a01724f5d4a2a171239573c4f7f509202
Johannes Spielmann <j@spielmannsolutions.com>
2015-04-25 14:47:36 +0200

    nur aktuelle Auktionen auf der Hauptseite

--- minibay/auctions/views.py
+++ minibay/auctions/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 from .models import Auction
 
@@ -6,7 +7,7 @@ from .models import Auction
 def main(request):
     context = {}
 
-    auctions = Auction.objects.all()
+    auctions = Auction.objects.filter(end_date__gt=timezone.now())
     context['auctions'] = auctions
     
     return render(request, 'auctions/main.djhtml', context)
